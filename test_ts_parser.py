dea7c7a412200ac1d0ecb584aaa45fc3331e83f2
junggu.lee <junggu.lee@lge.com>
2015-04-08 18:03:01 +0900

    테스트 코드를 refactorying

--- test_ts_parser.py
+++ test_ts_parser.py
@@ -1,28 +1,30 @@
 import unittest
 from ts_parser import TsParser
 
-class TestTsParser(unittest.TestCase):
-	def test_init(self):
-		# parser = TsParser()
-		# self.assertIsInstance(parser, TsParser)
+TS_FILE_NAME = '2_HDForum_H264.ts'
 
-		TS_FILE_NAME = '2_HDForum_H264.ts'
-		parser = TsParser(TS_FILE_NAME)
+class TestTsParser(unittest.TestCase):
+	def setUp(self):
+		self.parser = TsParser(TS_FILE_NAME)
+
+	def test_init(self):
+		self.assertIsInstance(self.parser, TsParser)
+		self.assertEqual(self.parser.ts_filename, TS_FILE_NAME)
+		self.assertIsNotNone(self.parser.handle)
+
+	@unittest.skip("I don't know how to check number of parameters")
+	def test_init_without_filename(self):
+		parser = TsParser()
 		self.assertIsInstance(parser, TsParser)
-		self.assertEqual(parser.ts_filename, TS_FILE_NAME)
-		self.assertIsNotNone(parser.handle)
 
 	def helper_loop_read(self):
-		TS_FILE_NAME = '2_HDForum_H264.ts'
-		parser = TsParser(TS_FILE_NAME)
+		self.parser = TsParser(TS_FILE_NAME)
 
 		while True:
-			packet = parser.read_one_packet()
+			packet = self.parser.read_one_packet()
 
 	def test_read_one_packet(self):
-		TS_FILE_NAME = '2_HDForum_H264.ts'
-		parser = TsParser(TS_FILE_NAME)
-		packet = parser.read_one_packet()
+		packet = self.parser.read_one_packet()
 		self.assertEqual(0x47, packet[0])
 
 	def test_raise_exception_if_packet_is_less_size(self):
