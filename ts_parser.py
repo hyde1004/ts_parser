40f92d392cd70127008b41202265f4c72ce28da1
junggu.lee <junggu.lee@lge.com>
2015-04-08 14:43:03 +0900

    class와 script로 분리

--- ts_parser.py
+++ ts_parser.py
@@ -1,50 +1,56 @@
-PACKET_SIZE = 188
-HEADER_SIZE = 4
+class TsParser:
+	pass
 
-def printHeader(packet):
-	for i in range(HEADER_SIZE):
-		print(format(packet[i], '#04x'), end=' ')
+if __name__ == '__main__':
+	PACKET_SIZE = 188
+	HEADER_SIZE = 4
 
-	print('')
+	def printHeader(packet):
+		for i in range(HEADER_SIZE):
+			print(format(packet[i], '#04x'), end=' ')
 
-def getBits(oneByte, bitPos, numOfBit):
-	convToBin = bin(oneByte)[2:].zfill(8)
-	bits = convToBin[bitPos:bitPos+numOfBit]
-	return int(bits, 2)
-
-def parseHeader(packet):
-	sync_byte = packet[0]
-	tei = (packet[1] & 0x80) >> 7 == 1
-	pusi = (packet[1] & 0x40) >> 6 == 1
-	tp_priority = (packet[1] & 0x20) >> 5
-	pid = ( (packet[1] & 0x1f) << 8 ) | packet[2] 
-	tsc = (packet[3] & 0xC0) >> 6
-	afc = (packet[3] & 0x30) >> 4
-	continuity_counter = packet[3] & 0x0f
-
-	if pid == 0:
-		print('sync_byte : ', format(sync_byte, '#04x'))
-		print('tei : ', tei)
-		print('pusi : ', pusi)
-		print('tp_priority : ', tp_priority)
-		print('pid : ', format(pid, '#04x'))
-		print('tsc : ', tsc)
-		print('afc : ', bin(afc))
-		print('continuity_counter : ', continuity_counter)
 		print('')
 
-try:
-	f = open('2_HDForum_H264.ts', 'rb')
+	def getBits(oneByte, bitPos, numOfBit):
+		convToBin = bin(oneByte)[2:].zfill(8)
+		bits = convToBin[bitPos:bitPos+numOfBit]
+		return int(bits, 2)
 
-	while True:
-		readOnePacket = f.read(PACKET_SIZE)
+	def parseHeader(packet):
+		sync_byte = packet[0]
+		tei = (packet[1] & 0x80) >> 7 == 1
+		pusi = (packet[1] & 0x40) >> 6 == 1
+		tp_priority = (packet[1] & 0x20) >> 5
+		pid = ( (packet[1] & 0x1f) << 8 ) | packet[2] 
+		tsc = (packet[3] & 0xC0) >> 6
+		afc = (packet[3] & 0x30) >> 4
+		continuity_counter = packet[3] & 0x0f
 
-		if len(readOnePacket) != PACKET_SIZE:
-			raise IndexError
+		if pid == 0:
+			print('sync_byte : ', format(sync_byte, '#04x'))
+			print('tei : ', tei)
+			print('pusi : ', pusi)
+			print('tp_priority : ', tp_priority)
+			print('pid : ', format(pid, '#04x'))
+			print('tsc : ', tsc)
+			print('afc : ', bin(afc))
+			print('continuity_counter : ', continuity_counter)
+			print('')
 
-	f.close()
-except IOError:
-	print("File isn't exist!")
-except IndexError:
-	print("End of file")
+	try:
+		f = open('2_HDForum_H264.ts', 'rb')
+
+		while True:
+			readOnePacket = f.read(PACKET_SIZE)
+
+			if len(readOnePacket) != PACKET_SIZE:
+				raise IndexError
+
+			parseHeader(readOnePacket)
+
+		f.close()
+	except IOError:
+		print("File isn't exist!")
+	except IndexError:
+		print("End of file")
 
