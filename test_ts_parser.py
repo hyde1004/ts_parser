acdd798fea87c292702760b836709b85b8e78e0c
junggu.lee <junggu.lee@lge.com>
2015-04-08 16:56:13 +0900

    ts 파일 끝 예외처리 테스트 코드 추가

--- test_ts_parser.py
+++ test_ts_parser.py
@@ -12,11 +12,23 @@ class TestTsParser(unittest.TestCase):
 		self.assertEqual(parser.ts_filename, TS_FILE_NAME)
 		self.assertIsNotNone(parser.handle)
 
+	def help_loop_read(self):
+		TS_FILE_NAME = '2_HDForum_H264.ts'
+		parser = TsParser(TS_FILE_NAME)
+
+		while True:
+			parser.read_one_packet()
+			if len(parser.packet) != 188:
+				raise ZeroDivisionError
+
+
 	def test_read_one_packet(self):
 		TS_FILE_NAME = '2_HDForum_H264.ts'
 		parser = TsParser(TS_FILE_NAME)
 		parser.read_one_packet()
 		self.assertEqual(0x47, parser.packet[0])
-		
+
+		self.assertRaises(ZeroDivisionError, self.help_loop_read )
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
